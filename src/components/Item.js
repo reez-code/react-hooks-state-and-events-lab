2d9c60b2ff87b9d753b7aaa863e41ebbd806833a
Phil <phil.github@gmail.com>
2024-04-27 19:31:57 +0300

    assignment completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [itemState, setItemState] = useState(false);
+
+  const listClassName = itemState ? "in-cart" : "";
+
+  function stateHandler() {
+    setItemState((currentState) => !currentState);
+  }
+
   return (
-    <li className="">
+    <li className={listClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={stateHandler}>
+        {itemState ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  function category(event) {
+    setSelectedCategory(event.target.value);
+  }
+  const categoryToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  });
+  const itemList = categoryToDisplay.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ));
   return (
-    <div className="ShoppingList">
+    <div className="ShoppingList" onChange={category}>
       <div className="Filter">
         <select name="filter">
           <option value="All">Filter by category</option>
@@ -12,11 +26,7 @@ function ShoppingList({ items }) {
           <option value="Dessert">Dessert</option>
         </select>
       </div>
-      <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
+      <ul className="Items">{itemList}</ul>
     </div>
   );
 }
